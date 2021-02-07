eca053b4b24d079915007de9ac1fb94cd7945618
Oleksandr Vodolazskyi <o_vodolazskyi@wargaming.net>
2021-02-07 18:08:59 +0100

    Add API documentation

--- docs/conf.py
+++ docs/conf.py
@@ -2,3 +2,8 @@
 project = "hypermodern-python-course"
 author = "Oleksandr Vodolazskyi"
 copyright = f"2021, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
